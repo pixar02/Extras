21dec4f76b9bbcbcda83ca8453f847d45369a016
BuildTools <unconfigured@null.spigotmc.org>
2016-05-11 15:18:14 +0200

    spawn and setspwan done

--- Extras/src/me/pixar02/Extras/Commands/setspawn.java
+++ Extras/src/me/pixar02/Extras/Commands/setspawn.java
@@ -1,8 +1,11 @@
 package me.pixar02.Extras.Commands;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import me.pixar02.Extras.Main;
 
@@ -16,10 +19,30 @@ public class setspawn implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		// TODO Auto-generated method stub
+		if(!(sender instanceof Player)){
+			sender.sendMessage(ChatColor.RED + "You have to be a player to perform this command!");
+		}else{
+			Player player = (Player) sender;
+			Location loc = player.getLocation();
+			if(plugin.getConfig().getString("spawn") == null){
+
+				plugin.getConfig().set("spawn.World", loc.getWorld().getName());
+				plugin.getConfig().set("spawn.X", loc.getX());
+				plugin.getConfig().set("spawn.Y", loc.getY());
+				plugin.getConfig().set("spawn.Z", loc.getZ());
+				plugin.getConfig().set("spawn.Yaw", loc.getYaw());
+				plugin.getConfig().set("spawn.Pitch", loc.getPitch());
+				sender.sendMessage(ChatColor.GREEN + "New Spawn has been set!");
+			}else{
+				plugin.getConfig().set("spawn.World", loc.getWorld().getName());
+				plugin.getConfig().set("spawn.X", loc.getX());
+				plugin.getConfig().set("spawn.Y", loc.getY());
+				plugin.getConfig().set("spawn.Z", loc.getZ());
+				plugin.getConfig().set("spawn.Yaw", loc.getYaw());
+				plugin.getConfig().set("spawn.Pitch", loc.getPitch());
+				sender.sendMessage(ChatColor.GREEN + "New Spawn has been set!" + ChatColor.RED + " (You overrote the last spawn)");
+			}
+		}
 		return false;
 	}
-	
-	
-	
 }
--- Extras/src/me/pixar02/Extras/Commands/spawn.java
+++ Extras/src/me/pixar02/Extras/Commands/spawn.java
@@ -1,6 +1,9 @@
 package me.pixar02.Extras.Commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,7 +23,20 @@ public class spawn implements CommandExecutor {
 		if(!(sender instanceof Player)){
 			sender.sendMessage(ChatColor.RED + "You have to be a player to perform this command!");
 		}else{
-			
+			Player player = (Player) sender;
+			if(plugin.getConfig().getString("spawn") == null){
+				sender.sendMessage(ChatColor.RED + "No Spawn set!");
+			}else{
+				World world = Bukkit.getWorld(plugin.getConfig().getString("spawn.World"));
+				double x = plugin.getConfig().getDouble("spawn.X");
+				double y = plugin.getConfig().getDouble("spawn.Y");
+				double z = plugin.getConfig().getDouble("spawn.Z");
+				int yaw = plugin.getConfig().getInt("spawn.Yaw");
+				int pitch = plugin.getConfig().getInt("spawn.Pitch");
+				Location loc = new Location(world, x, y, z, yaw, pitch);
+				player.teleport(loc);
+
+			}
 		}
 		return false;
 	}
